b4f91ee47bc143b5356b20b60768e01c8345689e
programmerAju <programmerhead7@gmail.com>
2023-12-17 21:22:11 +0530

    header and section1 created

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import '../styles/modern_normalize.css';
 import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
 import '../styles/utils.css';
 
