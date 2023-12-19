5ae262812615db5d2662cd7d98b97df127bd74e2
programmerAju <programmerhead7@gmail.com>
2023-12-19 22:53:02 +0530

    completed about section

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import '../styles/modern_normalize.css';
 import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
+import '../styles/components/about.css';
 import '../styles/utils.css';
 
